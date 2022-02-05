dfda3a0addc96123b0259606f9cca4eb30acf1f5
sjimenez <sjimenez@paradigmadigital.com>
2022-02-06 00:25:46 +0100

    fix

--- example-00/sample.tf
+++ example-00/sample.tf
@@ -2,12 +2,18 @@
 ##Este ejemplo crea una instancia de VM con Debian 9 de tamaño f1-micro en europe-west1-c 
 
 resource "google_compute_instance" "default" {
-    name         = "instance-vm-${random_id.instance_id.hex}"
+    name         = "instance-vm-01"
     machine_type = "f1-micro"
     zone         = "europe-west1-c"
-    boot_disk {
-    initialize_params {
-        image = "debian-cloud/debian-9"
+      boot_disk {
+        initialize_params {
+          image = "debian-cloud/debian-9"
       }
      }
+      network_interface {
+        network = "default"
+          access_config {
+        // Ephemeral public IP
+      }
     }
+  }
