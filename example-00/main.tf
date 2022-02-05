40e14d5e25a865a295daae61b48610bb86815ec9
sjimenez <sjimenez@paradigmadigital.com>
2022-02-06 00:09:29 +0100

    fix

--- example-00/main.tf
+++ example-00/main.tf
@@ -14,7 +14,8 @@ resource "random_string" "number" {
 }
 
 resource "google_storage_bucket" "tf-state" {
-  name          = "terraform-101-state-${random_string.number.result}"
+  #name          = "terraform-101-state-${random_string.number.result}"
+  name          = "terraform-101-state-183"
   location      = "EU"
   force_destroy = false
 # Es recomendable activar el versioning para los state files para poder hacer roll-back a una versión anterior.
--- example-00/sample.tf
+++ example-00/sample.tf
@@ -1,14 +1,13 @@
 # Descomentar una vez el storage este creado para configurar el remote state.
 ##Este ejemplo crea una instancia de VM con Debian 9 de tamaño f1-micro en europe-west1-c 
 
-#resource "google_compute_instance" "default" {
-# name         = "instance-vm-${random_id.instance_id.hex}"
-# machine_type = "f1-micro"
-# zone         = "europe-west1-c"
-
-# boot_disk {
-#   initialize_params {
-#     image = "debian-cloud/debian-9"
-#   }
-# }
-#}
+resource "google_compute_instance" "default" {
+    name         = "instance-vm-${random_id.instance_id.hex}"
+    machine_type = "f1-micro"
+    zone         = "europe-west1-c"
+    boot_disk {
+    initialize_params {
+        image = "debian-cloud/debian-9"
+      }
+     }
+    }
