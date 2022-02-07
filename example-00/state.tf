f910307a6f8eeba7634e27396a31896c8a1fe7d8
sjimenez <sjimenez@paradigmadigital.com>
2022-02-07 10:41:26 +0100

    rename

--- example-00/state.tf
+++ example-00/state.tf
@@ -1,8 +1,8 @@
 # Descomentar una vez el storage este creado para configurar el remote state.
-terraform {
-  backend "gcs" {
-    #Sustituir el nombre del bucket creado en el main.tf previamente
-    bucket = "terraform-101-state-183"
-    prefix = "terraform/state/example-00/"
-  }
-}
+#terraform {
+#  backend "gcs" {
+#    #Sustituir el nombre del bucket creado en el main.tf previamente
+#    bucket = "terraform-101-state-183"
+#    prefix = "terraform/state/example-00/"
+#  }
+#}
