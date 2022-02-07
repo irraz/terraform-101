516c741c44bce58bc2d4c2008a9e1f27f60d4ebc
Jam4Cloud <65178233+irraz@users.noreply.github.com>
2022-02-07 11:14:14 +0100

    update comment

--- example-00/state.tf
+++ example-00/state.tf
@@ -1,7 +1,7 @@
 # Descomentar una vez el storage este creado para configurar el remote state.
 #terraform {
 #  backend "gcs" {
-#    #Sustituir el nombre del bucket creado en el main.tf previamente
+#    #Sustituir el nombre del bucket
 #    bucket = "{ NOMBRE_BUCKET }"
 #    prefix = "terraform/state/example-00/"
 #  }
